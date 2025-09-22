778d895147cfa94284cbfaef5739a0a48ddb5d07
Eesh Mishra <eeshmishra@hotmail.com>
2025-09-23 02:02:28 +0530

    update

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-<script src="https://unpkg.com/scrollreveal"></script>
-ScrollReveal({
-    reset: true,
-    distance: '100px',
-    duration: 2000,
-    delay: 0
+window.addEventListener('load', function() {
+  const preloader = document.getElementById('preloader');
+  if (preloader) {
+    setTimeout(function() {
+      preloader.style.opacity = '0'; 
+      preloader.addEventListener('transitionend', function() {
+        preloader.style.display = 'none'; 
+      });
+    }, 2000);
+  }
 });
-
-ScrollReveal().reveal('.container',{delay: 0 , origin: 'left'});
-// ScrollReveal().reveal('.section-left',{delay: 0 , origin: 'right'});
