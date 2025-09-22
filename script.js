b06fb072505c64d77ef4a6208b132edde186c1c6
Eesh Mishra <eeshmishra@hotmail.com>
2025-09-22 23:22:12 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+<script src="https://unpkg.com/scrollreveal"></script>
+ScrollReveal({
+    reset: true,
+    distance: '100px',
+    duration: 2000,
+    delay: 0
+});
+
+ScrollReveal().reveal('.container',{delay: 0 , origin: 'left'});
+// ScrollReveal().reveal('.section-left',{delay: 0 , origin: 'right'});
